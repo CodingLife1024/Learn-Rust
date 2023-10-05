ab8a427ba5dac2400cb5e54d80ca6f110b8e7c6b
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-06 01:33:07 +0530

    erge

--- src/part5.rs
+++ src/part5.rs
@@ -1,4 +1,7 @@
+use std::io;
+
 fn main() {
-    const SECONDS_IN_MINUTE: u32 = 60;
-    println!("{}", SECONDS_IN_MINUTE);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("failed to read line");
+    println!("The input is: {}", input);
 }
