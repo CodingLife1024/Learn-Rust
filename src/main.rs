e7c1dcb6d5e8809a4e305c7347418b94425e2460
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-06 00:50:38 +0530

    3ger

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,6 @@ fn main() {
     let x = x + 65;
     println!("x is: {}", x);
 
+    const SECONDS_IN_MINUTE: u32 = 60;
+    println!("{}", SECONDS_IN_MINUTE);
 }
--- src/part3.rs
+++ src/part3.rs
@@ -1,4 +1,4 @@
 fn main() {
     const SECONDS_IN_MINUTE: u32 = 60;
-
+    println!("{}", SECONDS_IN_MINUTE);
 }
