a8596c0bfeb2c500a112c875abfc3fdfae8e13d8
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-06 00:46:32 +0530

    wgw

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
     let x: i32 = 4;
     println!("x is: {}", x);
+
+    {
+        let x = 6;
+        println!("x is: {}", x);
+    }
     let x = x + 65;
     println!("x is: {}", x);
 
