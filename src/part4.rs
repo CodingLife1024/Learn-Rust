a59bcd43feba2f5e85cdbe25fa19d6e1578773f7
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-06 01:24:04 +0530

    thet

--- src/part4.rs
+++ src/part4.rs
@@ -15,4 +15,8 @@ fn main() {
 
     let tup: (i32, bool, char) = (1, true, 'd');
     println!("{}", tup.0);
+
+    let mut arr: [i32; 6] = [1, 2, 3, 4, 5, 6];
+    arr[4] = 10;
+    println!("{}", arr[4]);
 }
