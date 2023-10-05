425a7cd741d12ea1d7e7cea7795cbd52499e0cbe
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-06 01:11:20 +0530

    gre

--- src/part4.rs
+++ src/part4.rs
@@ -6,4 +6,13 @@ fn main() {
     // i64
     // i128
     println!("{}", x);
+    // f32
+    // f64
+
+    // bool
+
+    //char
+
+    let tup: (i32, bool, char) = (1, true, 'd');
+    println!("{}", tup.0);
 }
