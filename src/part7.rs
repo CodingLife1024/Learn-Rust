de815b0c8b4a4178eceb023533d0f8c659c7adc0
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-11 02:24:19 +0530

    gerg

--- src/part7.rs
+++ src/part7.rs
@@ -1,4 +1,16 @@
-fn main() {
-    let cond = (2 as f32) < 3.5;
-    println!("{}", cond);
+// fn main() {
+//     let cond = (2 as f32) < 3.5;
+//     let cond2 = false && cond;
+//     let cond3 = !(true || cond2);
+//     println!("{}", cond3);
+// }
+
+fn main(){
+    let food = "cookie";
+    if food == "cookie"{
+        println!("{}", "rrrr");
+    }
+    else{
+        println!("{}", "erfer");
+    }
 }
