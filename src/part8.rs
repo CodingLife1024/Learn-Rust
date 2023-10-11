5f751a50ea44584cf67d6633c2f95b2c2816526d
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-10-12 00:55:05 +0530

    fgr

--- src/part8.rs
+++ src/part8.rs
@@ -1,7 +1,19 @@
 fn main() {
     println!("{}", "heyy world !!!");
+    test_one();
+    add_numbers(2, 44);
+
+    let number = {
+        let x = 3;
+        x + 1 
+    };
+    println!("{}", number);
 }
 
 fn test_one() {
-    
+    println!("Test has been called...")
+}
+
+fn add_numbers(x: i32, y: i32) {
+    println!("The sum is: {}", x + y);
 }
